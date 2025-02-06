1e00524b0cfbbee74b19be4238b58cbf6916dcd8
NKJ14 <nishanthere1402@gmail.com>
2025-02-06 22:56:08 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div class="bg-red-500">
+      Hello
+    </div>
+  )
+}
+
+export default App
